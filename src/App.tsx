37806bc77d755c0f934ff13d36f5c5176c725aa8
DongYeopLee <eaea7314@gmail.com>
2026-01-23 22:27:33 +0900

    [add]: ng2react ci cd 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
-        <p>Update Ng2react Project</p>
+        <p>Update Ng2react Project Test</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
